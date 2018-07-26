febb9a8c48034c22083ed888a18914755125c3ef
Hai Tran <tuanhai.dev@gmail.com>
2018-07-26 23:32:41 +0700

    Initial Commit

---
+++ RideShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RideShare
+//
+//  Created by Tran Tuan Hai on 7/26/18.
+//  Copyright © 2018 Tran Tuan Hai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
