d8df4962c9a866cd2985c5f6b9542210eadeb0c9
Hai Tran <tuanhai.dev@gmail.com>
2018-08-07 07:54:59 +0700

    Update CircleView

--- RideShare/View/CircleView.swift
+++ RideShare/View/CircleView.swift
@@ -3,6 +3,8 @@ import UIKit
 class CircleView: UIView {
   func setupView() {
     layer.cornerRadius = bounds.width / 2
+    layer.borderWidth = 1.5
+    layer.borderColor = UIColor.black.cgColor
   }
   
   override func awakeFromNib() {
