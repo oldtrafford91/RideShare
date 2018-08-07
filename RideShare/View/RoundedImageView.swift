0d784446b67a76ce88a6a11581c5198eba7ee962
Hai Tran <tuanhai.dev@gmail.com>
2018-08-07 07:04:36 +0700

    Clean up RoundedImageView

--- RideShare/View/RoundedImageView.swift
+++ RideShare/View/RoundedImageView.swift
@@ -1,23 +1,13 @@
-//
-//  RoundedImageView.swift
-//  RideShare
-//
-//  Created by Tran Tuan Hai on 7/29/18.
-//  Copyright © 2018 Tran Tuan Hai. All rights reserved.
-//
-
 import UIKit
 
 @IBDesignable
 class RoundedImageView: UIImageView {
   
   @IBInspectable var cornerRadius: CGFloat = 2.0 {
-    
     didSet {
       layer.cornerRadius = cornerRadius
       layer.masksToBounds = true
     }
-    
   }
   
   override func awakeFromNib() {
@@ -29,7 +19,4 @@ class RoundedImageView: UIImageView {
     layer.cornerRadius = bounds.width / 2
     layer.masksToBounds = true
   }
-  
-  
-  
 }
