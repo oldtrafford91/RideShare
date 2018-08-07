b2db15e4501c3e63b6a8d2da123f475379ca9d0b
Hai Tran <tuanhai.dev@gmail.com>
2018-08-07 07:06:37 +0700

    Clean up ViewController

--- RideShare/Controller/ViewController.swift
+++ RideShare/Controller/ViewController.swift
@@ -1,29 +1,13 @@
-//
-//  ViewController.swift
-//  RideShare
-//
-//  Created by Tran Tuan Hai on 7/26/18.
-//  Copyright © 2018 Tran Tuan Hai. All rights reserved.
-//
-
 import UIKit
 import MapKit
 
 class ViewController: UIViewController {
-  
   @IBOutlet weak var mapView: MKMapView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
   }
-
-
 }
 
 extension ViewController: MKMapViewDelegate {
-  
 }
-
-
-
