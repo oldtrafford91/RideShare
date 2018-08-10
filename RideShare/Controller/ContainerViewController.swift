3aa97e7388fc41e7fa00945786593ea07b815596
Hai Tran <tuanhai.dev@gmail.com>
2018-08-10 08:05:50 +0700

    Extend UIStoryboard with function to easily instantiate View Controller from Storyboard

--- RideShare/Controller/ContainerViewController.swift
+++ RideShare/Controller/ContainerViewController.swift
@@ -2,3 +2,15 @@ import UIKit
 
 class ContainerViewController: UIViewController {
 }
+
+private extension UIStoryboard {
+  class func mainStoryboard() -> UIStoryboard {
+    return UIStoryboard(name: "Main", bundle: .main)
+  }
+  class func leftSidePanelViewController() -> LeftSidePanelViewController? {
+    return mainStoryboard().instantiateViewController(withIdentifier: "LeftSidePanelViewController") as? LeftSidePanelViewController
+  }
+  class func homeViewController() -> HomeViewController? {
+    return mainStoryboard().instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController
+  }
+}
